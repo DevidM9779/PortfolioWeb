9a6b78f54c310ed839c20b4165673337f4f74a29
David Milan-Medina <ddavidmilan@hotmail.es>
2022-11-16 20:30:50 -0600

    First commit: Navbar fully operative

---
+++ src/components/CircleLogo/CircleLogo.jsx
@@ -0,0 +1,8 @@
+import "./CircleLogo.css"
+
+const CircleLogo = () => {
+    return <div className="circle"/>
+}
+
+
+export default CircleLogo;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import CircleLogo from "../CircleLogo/CircleLogo";
+import './Navbar.css'
+
+export const Navbar = () => {
+    return(
+
+            <div className='navbar-container'>
+                <div className='navbar'>
+
+                    <CircleLogo/>
+                    <div className={'navbar-menu'}>
+                        <div className={'navbar-item'}>About</div>
+                        <div className={'navbar-item'}>Projects</div>
+                        <div className={'navbar-item'}>Contact</div>
+                    </div>
+
+                </div>
+            </div>
+
+    )
+}
