952eb867fedba5258de9927b67edee705ce01d0c
David Milan-Medina <ddavidmilan@hotmail.es>
2022-12-29 07:35:05 -0600

    About page updated + Pictures added to public folder and about page

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -6,7 +6,7 @@ export const AboutSection = () => {
         <div className={"about-container"} id={"about"}>
             <div className={"section-body"}>
                 <p>
-                    Welcome to my portfolio website! My name is David and I am a recent graduate with a degree in computer science and mathematics.
+                    Welcome to my portfolio website! My name is <b>David</b> and I am a recent graduate with a degree in computer science and mathematics.
                 </p>
                 <p>
                     I have a strong foundation in software development, with experience in a variety of programming languages including java, javascript, python, c#, sql, swiftui...
@@ -19,7 +19,9 @@ export const AboutSection = () => {
                     I am excited to begin my career as a software developer and am eager to apply my skills and knowledge to new challenges. <u>Thank you</u> for visiting my portfolio website and I hope you find my work interesting and relevant.
                 </p>
             </div>
-
+            <div className={"about-me-picture"}>
+                <img src="/undraw_ideas_re_7twj.svg" alt="About Me" height="200px" width="200px"></img>
+            </div>
         </div>
     )
 }
